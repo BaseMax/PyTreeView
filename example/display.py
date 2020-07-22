9f494fc231d0fc2234e00652f37639338efb7e9b
Max Base <MaxBaseCode@Gmail.Com>
2020-07-22 15:33:39 +0430

    Update display.py

--- example/display.py
+++ example/display.py
@@ -1,4 +1,4 @@
 from PyTreeView import PyTreeView
 
 result={'imports': [[['net', 'socket']], [['sys'], ['json'], ['xml']]], 'statements': [], 'module': 'main'}
-pyTree().display(result)
+PyTreeView().display(result)
