570e917cb090ccc6eb040da42b55b511579909db
Max Base <MaxBaseCode@Gmail.Com>
2020-07-22 14:44:35 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,32 @@
+# pytree
+# TODO
+import pathlib
+from setuptools import setup
 
+HERE = pathlib.Path(__file__).parent
+README = (HERE/"README.md").read_text()
+
+setup(
+   name="pytree", 
+   version="1.0.0", 
+   descp=" pyTree is a python class and library to display Tree (Tree View) from array, object, or JSON.", 
+   long_descp=README,
+   long_descp_content="text/markdown", 
+   URL="https://github.com/basemax/pytree", 
+   author="Max Base", 
+   authoremail="maxbasecode@gmail.com", 
+   license="MIT",
+   classifiers=[ 
+        "License :: OSI Approved :: MIT License", 
+        "Programming Language :: Python :: 3", 
+        "Programming Language :: Python :: 3.7", 
+   ], 
+   packages=["pytree"], 
+   includepackagedata=True, 
+   installrequires=[], 
+   entrypoints={ 
+       "console_scripts":[ 
+           "realpython=pytree", #TODO 
+       ] 
+   }, 
+ )
